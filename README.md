ff0be8a501fefc4b29009c4fa9dce0b0cabb4e00
Ali <alouloucheikh05@gmail.com>
2024-04-21 20:25:06 +0100

    fist commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Server Whitelist Application
+<img src='src/img.png' width='100%'>
+Server Whitelist Application is a web form designed for users to apply for whitelisting on a Minecraft server.
+
+## Features
+
+<img src="src/computer.gif" alt="Computer" width="500" align="right"/>
+
+- **User-friendly Interface**: Easy-to-use form for submitting whitelist applications.
+- **Responsive Design**: Works seamlessly across various devices.
+- **Acceptance of Rules**: Includes a checkbox to confirm the acceptance of server rules.
+- **Additional Information**: Allows users to provide additional information along with their application.
+
+## Technologies Used
+
+<p align="center">
+  <img src="https://img.shields.io/badge/HTML-5-orange"/>
+  <img src="https://img.shields.io/badge/CSS-3-blue"/>
+  <img src="https://img.shields.io/badge/JavaScript-ES6-yellow"/>
+  <img src="https://img.shields.io/badge/jQuery-3.6.0-blue"/>
+  <img src="https://img.shields.io/badge/jQuery%20UI-1.12.1-blue"/>
+  <img src="https://img.shields.io/badge/Swal-10-blue"/>
+  <img src="https://img.shields.io/badge/Google%20Sheets-API-green"/>
+  <img src="https://img.shields.io/badge/Google%20Apps%20Script-API-yellowgreen"/>
+  <img src="https://img.shields.io/badge/Google%20Translate-API-brightgreen"/>
+</p>
+
+## Usage
+
+1. Fill out the form with your Minecraft username, Discord name, server playtime, etc.
+2. Accept the server rules by clicking on the appropriate checkbox.
+3. Click the "SUBMIT" button to submit your application.
+4. Upon successful submission, you'll receive a success message.
+5. after successfully submitting, you will see the download mods button to get your mods from.
+6. If you have any questions or issues, please contact the server owner.
+
+## Contact Information
+
+- **Discord**: [Ali Cheikh](https://discord.gg/jRbrRwEzjH)
+- **Planet Minecraft**: unexpected14
+
+## Support
+
+- [FAQs](#)
+- [Terms of Service](#)
+- [Privacy Policy](#)
+
+## Donate
+
+Support the server by donating! [Donate Now](src/payto.pdf)
+
+---
+made this for fun but might be the new whitelist for [Play.minecraft.tn](https://minecraft.tn)
+© 2024 [Ali Cheikh](bit.ly/Aliportfolio). Tunisian server. All rights reserved.
---
+++ script.js
@@ -0,0 +1,120 @@
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+  }
+document.getElementById('whitelistForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitFormToGoogleSheets();
+    document.getElementById('whitelistForm').reset(); // Reset form fields
+    document.getElementById('successMessage').style.display = 'block'; // Show success message
+});
+
+document.querySelectorAll('.bannedCheckbox').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      // Uncheck all checkboxes except the one that was just clicked
+      document.querySelectorAll('.bannedCheckbox').forEach(function(otherCheckbox) {
+        if (otherCheckbox !== checkbox) {
+          otherCheckbox.checked = false;
+        }
+      });
+
+      // Disable the bannedReason input if "No" is checked
+      document.getElementById('bannedReason').disabled = document.getElementById('bannedNo').checked;
+    });
+  });
+  document.getElementById('bannedYes').addEventListener('change', function() {
+  if (this.checked) {
+      document.getElementById('bannedReasonContainer').style.display = 'none';
+      document.getElementById('bannedReason').disabled = true;
+  }
+  });
+
+  document.getElementById('bannedNo').addEventListener('change', function() {
+  if (this.checked) {
+      document.getElementById('bannedReasonContainer').style.display = 'block';
+      document.getElementById('bannedReason').disabled = false;
+  }
+  });
+
+  function displayAlert() {
+    
+    document.getElementById('submit').style.display = 'block';
+    document.getElementById('sayee').style.display = 'block';
+    Swal.fire({
+        title: 'You must accept the rules.',
+        html: `<a href='rules.html'><img src="https://cdn.discordapp.com/attachments/967158057744355328/1231681841107435590/rIiHZA2.png?ex=6626b49a&is=6625631a&hm=74cd98eeeb69e9b937afd78d7d5b37e4c3d19b216581c55568c4aeaf069cd839&" style="max-width: 100%;" alt="Rules"></a>`,
+        icon: 'warning',
+        confirmButtonText: 'OK',
+        showConfirmButton: false,
+    });
+}
+
+function submitFormToGoogleSheets() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxb7QrRcEKCjMllb76hUgNjbVUkbd8a5bJW7cY-HzCedHq-o-68RIhvWBQy4r72bg/exec';
+    const form = document.getElementById('whitelistForm');
+    Swal.fire({
+        icon: 'info',
+        title: 'Submitting...',
+        text: 'Sending whitelist application request',
+        showConfirmButton: false,
+        allowOutsideClick: false
+    });
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            if (response.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success!',
+                    html: `
+                    <h3 style="color:white">
+                    Your application is submitted.
+                    <u> Thank you!</u>
+                    </h3>
+                    `,
+                    timer: 3000,
+                    background: "url(src/download.png)",
+                    timerProgressBar: true,
+                    showConfirmButton: false,
+                    allowOutsideClick: true
+                });
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+        })
+        .catch(error => {
+            console.error('Error!', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong! Please try again later.'
+            });
+        });
+}
+
+document.addEventListener('scroll', function() {
+    var footerPosition = document.querySelector('footer').getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (footerPosition < windowHeight) {
+        document.body.classList.add('footer-reached');
+    } else {
+        document.body.classList.remove('footer-reached');
+    }
+});
+
+function alertt(){
+    document.getElementById('submit').style.display = 'none';
+    Swal.fire({
+        icon: 'info',
+        title: 'You must accept the rules.',
+        html: `
+        <h6>tijmo til9aw il rules mta3 server fi discord wila</h6>
+        <h5>
+        <a href="rules.html" class="a1">#here</a>
+        </h5>
+        ` ,
+        showConfirmButton: false,
+        allowOutsideClick: false
+    });
+}
